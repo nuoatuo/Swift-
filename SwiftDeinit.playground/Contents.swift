1a6859be542b5a4e5d7a64c0a7d1671e42c7398d
nuoatuo <nuoatuo@126.com>
2016-09-30 13:42:03 +0800

    添加析构函数事例
    添加析构函数事例

--- SwiftDeinit.playground/Contents.swift
+++ SwiftDeinit.playground/Contents.swift
@@ -3,3 +3,15 @@
 import UIKit
 
 var str = "Hello, playground"
+
+class Person {
+    var name: String = ""
+    var  age: Int = 0
+    
+    //重写析构函数，监听对象的销毁
+    deinit {
+        print("Person----deinit")
+    }
+}
+var p: Person? = Person()
+p = nil
